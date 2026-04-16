7401a719834ff81ba57e96f180ca736d54c609c0
Muhammad Anique <165008080+Anique-1@users.noreply.github.com>
2026-04-16 10:01:54 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,7 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 import sys
 import platform
-from setuptools import setup, Extension
-
-# Determine if the current platform is macOS on ARM
-is_mac_arm = sys.platform == 'darwin' and platform.machine() in ['arm64', 'aarch64']
-
-# Base arguments
-if sys.platform == 'win32':
-    compile_args = ['/O2', '/openmp', '/arch:AVX2', '/std:c++17']
-    link_args = ['/openmp']
-else:
-    compile_args = ['-O3', '-std=c++17']
-    # Add AVX2 for non-ARM macOS and Linux
-    if not is_mac_arm:
-        compile_args.append('-mavx2')
-    # OpenMP is typically not available by default on macOS clang
-    if sys.platform == 'linux':
-        compile_args.append('-fopenmp')
-        
-    link_args = ['-fopenmp'] if sys.platform == 'linux' else []
 
 class get_pybind_include(object):
     def __init__(self, user=False):
@@ -27,6 +10,22 @@ class get_pybind_include(object):
     def __str__(self):
         import pybind11
         return pybind11.get_include(self.user)
+
+# Determine if the architecture supports AVX2 (x86/x64)
+machine = platform.machine().lower()
+is_x86 = machine in ['x86_64', 'amd64', 'x86', 'i386', 'i686']
+
+# Configure compile arguments based on platform and architecture
+if sys.platform == 'win32':
+    compile_args = ['/O2', '/openmp', '/std:c++17']
+    if is_x86:
+        compile_args.append('/arch:AVX2')
+else:
+    compile_args = ['-O3', '-std=c++17']
+    if is_x86:
+        compile_args.append('-mavx2')
+    if sys.platform == 'linux':
+        compile_args.append('-fopenmp')
 
 ext_modules = [
     Extension(
@@ -38,14 +37,15 @@ ext_modules = [
             'include/'
         ],
         extra_compile_args=compile_args,
-        extra_link_args=link_args,
+        extra_link_args=['/openmp'] if sys.platform == 'win32' else 
+                        (['-fopenmp'] if sys.platform == 'linux' else []),
         language='c++'
     ),
 ]
 
 setup(
     name='fasttok',
-    version='0.1.1',
+    version='0.1.4',
     author='Anique',
     author_email='muhammadanique81@gmail.com',
     description='A high-performance C++ powered tokenizer and compressor for LLMs',
